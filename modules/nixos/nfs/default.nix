97c0e478bcad90f4d74a24f1a7eff0f9a2175d4c
alex <alex@mcculley.tech>
2023-09-22 17:40:56 -0500

    nfs fix for achilles

--- modules/nixos/nfs/default.nix
+++ modules/nixos/nfs/default.nix
@@ -1,13 +1,13 @@
 {
   fileSystems."/home/alex/ISOs" = {
     # mounted with tailscale DNS name
-    device = "100.80.253.105:/mnt/billthepony/proxmox/template/iso";
+    device = "truenas.nix.mcculley.tech:/mnt/billthepony/proxmox/template/iso";
     fsType = "nfs";
     options = ["rw" "soft" ];
   };
 
   fileSystems."/home/alex/Documents/Games" = {
-    device = "100.80.253.105:/mnt/billthepony/games";
+    device = "truenas.nix.mcculley.tech:/mnt/billthepony/games";
     fsType = "nfs";
     options = ["rw" "soft" ];
   };
