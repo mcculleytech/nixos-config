c0b2d11e9a87c65d5024e46ff643b686ba49c537
alex <alex@mcculley.tech>
2023-10-21 19:20:12 -0500

    aeneas fixes

--- hosts/aeneas/configuration.nix
+++ hosts/aeneas/configuration.nix
@@ -13,6 +13,20 @@
 
   services.hardware.bolt.enable = true;
 
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 }
