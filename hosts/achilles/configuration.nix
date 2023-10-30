958edaf27fb0bac78acc9fea93569bb9c54ffb24
alex <alex@mcculley.tech>
2023-10-30 16:20:32 -0500

    pipewire over pulse

--- hosts/achilles/configuration.nix
+++ hosts/achilles/configuration.nix
@@ -26,8 +26,18 @@
   services.xserver.layout = "us";
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
 
   programs.dconf.enable = true;
 
