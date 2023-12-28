70a0e3de8b415e7a4777999830c761372bf19709
alex <alex@mcculley.tech>
2023-12-28 10:07:18 -0600

    ssh config

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -10,4 +10,7 @@
       PasswordAuthentication = false;
     };
   };
+  users.users."alex".openssh.authorizedKeys.keys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBLAg2zXXAlqhi+wg1EaezH2TQW4rnQ0oULK6CnXyBS2AAAAD3NzaDpzeXN0ZW0tYXV0aA== ssh:system-auth"
+  ];
 }
