2619cbc68e0f3334e86c0aaf69f515aef9825788
alex <alex@mcculley.tech>
2024-02-04 10:26:36 -0600

    autoupgrade

--- hosts/common/global/nix-settings.nix
+++ hosts/common/global/nix-settings.nix
@@ -21,4 +21,15 @@
       allowUnfree = true;
     };
   };
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
 }
