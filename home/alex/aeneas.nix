f21ed64a79d86e1fbe900012770153cf9d2a309f
alex <alex@mcculley.tech>
2024-02-05 04:22:27 -0600

    aeneas shortcuts

--- home/alex/aeneas.nix
+++ home/alex/aeneas.nix
@@ -4,4 +4,11 @@
       ./global
     ];
 
+  programs.zsh = {
+    shellAliases= {
+      os-rebuild   = "export HOSTNAME=$(hostname); sudo nixos-rebuild switch --flake '/home/alex/Repositories/nixos-config/#aeneas'";
+      home-rebuild = "export HOSTNAME=$(hostname); home-manager switch --flake '/home/alex/Repositories/nixos-config/#alex@aeneas'";
+    };
+  };
+
 }
