518d0977f74bc9c43eeda7a8001023209437f64c
alex <alex@mcculley.tech>
2024-02-11 08:32:41 -0600

    hopeful for disko labels

--- disko/vader.nix
+++ disko/vader.nix
@@ -1,4 +1,4 @@
-{
+{ config, ... }: {
   disko.devices = {
     disk = {
       sda = {
@@ -10,40 +10,37 @@
             boot = {
               size = "1M";
               type = "EF02"; # for grub MBR
+              label = "boot";
             };
             ESP = {
               size = "512M";
               type = "EF00";
+              label = "ESP";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
-          };
+            };
             root = {
+              label = "${config.networking.hostName}";
               size = "100%";
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ]; # Override existing partition
-                # Subvolumes must set a mountpoint in order to be mounted,
-                # unless their parent is mounted
                 subvolumes = {
-                  # Subvolume name is different from mountpoint
                   "/root" = {
                     mountOptions = [ "compress=zstd" ];
                     mountpoint = "/";
                   };
-                  # Subvolume name is the same as the mountpoint
                   "/persist" = {
                     mountOptions = [ "compress=zstd" ];
                     mountpoint = "/persist";
                   };
-                  # Parent is not mounted so the mountpoint must be set
                   "/nix" = {
                     mountOptions = [ "compress=zstd" "noatime" ];
                     mountpoint = "/nix";
                   };
-                  # Subvolume for the swapfile
                   "/swap" = {
                     mountpoint = "/.swapvol";
                     swap = {
