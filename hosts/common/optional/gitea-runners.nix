a9023e4778e2f513cb6bacbe91b959eef993a214
alex <alex@mcculley.tech>
2024-02-10 18:27:21 -0600

    gitea runners with label

--- hosts/common/optional/gitea-runners.nix
+++ hosts/common/optional/gitea-runners.nix
@@ -5,7 +5,8 @@
       gitea_actions_token = {
       sopsFile = ../../vader/secrets.yaml;
       owner = config.systemd.services.gitea.serviceConfig.User;
-    };
+    	};
+	};
 
     sops.templates."gitea_actions_token".content = ''
       "${config.sops.placeholder.gitea_actions_token}"
@@ -14,16 +15,22 @@
 	services.gitea-actions-runner = {
 		instances = {
 			hugo = {
+				name = "hugo";
 				url = "https://source.mcculley.tech";
-				tokenFile = config.sops.secrets.gitea_actions_token.path;
+				tokenFile = config.sops.secrets."gitea_actions_token".path;
 				enable = true;
+				labels = [ 
+					"ubuntu:latest:docker" 
+					"native:host" 
+				];
 			};
-			flake-update = {
-				url = "https://source.mcculley.tech";
-				tokenFile = config.sops.secrets.gitea_actions_token.path;
-				enable = true;
-			};
+			# flake-update = {
+			# 	name = "flake-update";
+			# 	url = "https://source.mcculley.tech";
+			# 	tokenFile = config.sops.secrets."gitea_actions_token".path;
+			# 	enable = true;
+			# 	labels = ;
+			# };
 		};
 	};
-  };
 }
