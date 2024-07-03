cd6fdb9a242562f0ea209544238dc9df243fc4ac
alex <alex@mcculley.tech>
2024-07-02 20:00:21 -0500

    kde edit for remote access

--- hosts/common/optional/roles/workstation/kde.nix
+++ hosts/common/optional/roles/workstation/kde.nix
@@ -2,5 +2,11 @@
   services.xserver.enable = true;
   services.xserver.xkb.layout = "us";
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+
+   environment.systemPackages = with pkgs; 
+   [
+    kdePackages.krdc
+
+   ];
 }
