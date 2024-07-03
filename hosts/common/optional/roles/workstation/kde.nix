66e8ba18244501a5965eefdea0fe29d27e3c0eda
alex <alex@mcculley.tech>
2024-07-02 20:02:52 -0500

    kde edit for future syntax

--- hosts/common/optional/roles/workstation/kde.nix
+++ hosts/common/optional/roles/workstation/kde.nix
@@ -2,7 +2,7 @@
   services.xserver.enable = true;
   services.xserver.xkb.layout = "us";
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
    environment.systemPackages = with pkgs; 
    [
