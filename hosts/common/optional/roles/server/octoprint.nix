ab78513da48be7ba13894cd364ebac1f630a73c9
alex <alex@mcculley.tech>
2024-07-02 22:22:25 -0500

    octoprint stream revert

--- hosts/common/optional/roles/server/octoprint.nix
+++ hosts/common/optional/roles/server/octoprint.nix
@@ -5,15 +5,11 @@
 		openFirewall = true;
 	};
 
-
-	mjpg-streamer = {
-	  enable = true;
-	  # https://github.com/jacksonliam/mjpg-streamer/blob/master/mjpg-streamer-experimental/plugins/input_uvc/README.md
-	  inputPlugin = "input_uvc.so --fps 1 -timeout 120";
-	  outputPlugin = "output_http.so --www @www@ --nocommands --port 5050";
-	};
-
-	networking.firewall.allowedTCPPorts = [ 5050 ];
+	environment.systemPackages = with pkgs; 
+	[
+	  fswebcam
+	  ustreamer
+	];
 
 	environment.persistence = {
 	  "/persist" = {
