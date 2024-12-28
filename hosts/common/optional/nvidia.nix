203e76ca5784b0eaea62ce03c9518a9296622bfe
alex <alex@mcculley.tech>
2024-12-28 12:01:31 -0600

    playing around with nvidia options

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -1,28 +1,15 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
-
- services.xserver.videoDrivers = [ "nvidia" ];
-
-  #hardware.graphics.enable = true;
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-  
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    open = false;
-    nvidiaSettings = false;
+    # powerManagement.finegrained = false;
 
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "560.35.03";
-      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-      sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
-      openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
-      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-      persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
-    };
+    open = true;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
