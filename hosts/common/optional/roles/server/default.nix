62bd0c5efe8639a5fc985793896788129baa596a
alex <alex@mcculley.tech>
2024-12-28 12:49:56 -0600

    remote-backup-server option enable

--- hosts/common/optional/roles/server/default.nix
+++ hosts/common/optional/roles/server/default.nix
@@ -5,6 +5,7 @@
     ./octoprint.nix
     ./jellyfin.nix
     ./laptop-disable-suspend-on-close.nix
+    ./remote-backup-server.nix
     ./ollama.nix
     ./immich.nix
     ./qemu-config.nix
@@ -13,6 +14,6 @@
     ./gitea
     ./traefik
     ./tailscale-server.nix
-    ./syncthing-server.nix
+    ./syncthing-server.ni
   ];
 }
