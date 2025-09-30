9e4131532a9e31ec5934465d85cfdaaf6edef244
alex mcculley <65031130+mcculleytech@users.noreply.github.com>
2025-09-30 12:32:55 -0500

    Update nix-settings.nix

--- hosts/common/global/nix-settings.nix
+++ hosts/common/global/nix-settings.nix
@@ -8,9 +8,12 @@
       auto-optimise-store = true;
       substituters = [ 
         "https://cosmic.cachix.org/"
-        "https://cuda-maintainers.cachix.org/"
+        "https://nix-community.cachix.org"
        ];
-      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+      trusted-public-keys = [ 
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" 
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" 
+      ];
       trusted-users = ["root" "alex"];
     };
     gc = {
