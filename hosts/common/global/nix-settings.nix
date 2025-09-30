86196baa8297f2e769dd051af8ed19938fea4b2c
alex mcculley <65031130+mcculleytech@users.noreply.github.com>
2025-09-30 13:05:02 -0500

    Update nix-settings.nix

--- hosts/common/global/nix-settings.nix
+++ hosts/common/global/nix-settings.nix
@@ -1,7 +1,7 @@
 { lib, config, inputs, outputs, ... }: {
   nix = {
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.flake.to.path}") config.nix.registry;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.flake}") config.nix.registry;
 
     settings = {
       experimental-features = ["nix-command" "flakes"];
