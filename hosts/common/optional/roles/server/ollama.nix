2f8b9811fa27a02c3f0795c8a363c898206557b9
alex <alex@mcculley.tech>
2025-10-01 09:54:59 -0500

    ollama fix

--- hosts/common/optional/roles/server/ollama.nix
+++ hosts/common/optional/roles/server/ollama.nix
@@ -1,27 +1,4 @@
-{ pkgs, config, lib, ... }: 
-  # Have to add this for the 1080Ti since the arch is 6.1, nix doesn't have that in the prebuilt
- let
- custom-ollama-cuda = pkgs.ollama-cuda.overrideAttrs (old: rec {
-  buildPhase = ''
-     # Compute CUDA architectures manually for the build
-     cmake -B build \
-       -DCMAKE_SKIP_BUILD_RPATH=ON \
-       -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-       -DCMAKE_CUDA_ARCHITECTURES=61 \
-
-      cmake --build build -j $NIX_BUILD_CORES
-   '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp build/bin/app $out/bin/
-    # Copy libs
-    cp -r build/lib/* $out/lib/
-  '';
-
-  });
- in
-{
+{ pkgs, config, lib, ... }: {
 
   options = {
     ollama.enable =
@@ -31,7 +8,11 @@
   config = lib.mkIf config.ollama.enable {
 
     services.ollama = {
-      package = custom-ollama-cuda;
+      package = pkgs.unstable.ollama.override {
+        cudaArches = [
+          "61"
+        ];
+      };
       enable = true;
       acceleration = "cuda";
       host = "0.0.0.0";
