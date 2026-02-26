87ed3c1d465e5e09dd74cf7de56ea3b38017b3c2
alex mcculley <alex@DESKTOP-1QSWEGR.local>
2026-02-26 14:17:30 -0600

    acme cert management toggle configured

--- hosts/common/optional/roles/server/acme.nix
+++ hosts/common/optional/roles/server/acme.nix
@@ -1,40 +1,48 @@
 { config, ...}: {
 
-	sops.secrets = {
-	  cloudflare_email = {
-	    sopsFile = ../../../../atreides/secrets.yaml;
-	  };
-	  cloudflare_api_key = {
-	    sopsFile = ../../../../atreides/secrets.yaml;
-	  };
-	};
-	networking.firewall.allowedTCPPorts = [ 80 443 ];
+  options = {
+    acme.enable =
+      lib.mkEnableOption "enables acme certificate management.";
+  };
 
-	security.acme = {
-		acceptTerms = true;
-		defaults = {
-			email = "alex@mcculley.tech";
-			dnsProvider = "cloudflare";
-			credentialFiles = {
-				CLOUDFLARE_EMAIL_FILE = config.sops.secrets.cloudflare_email.path;
-				CLOUDFLARE_API_KEY_FILE = config.sops.secrets.cloudflare_api_key.path;
-			};
-		};
-		certs = {
-			"home.mcculley.tech" = {
-			  inheritDefaults = true;
-			  domain = "*.home.mcculley.tech";
-			  extraDomainNames = ["home.mcculley.tech"];
-			};
-		};
-	};
+  config = lib.mkIf config.acme.enable {
 
-	environment.persistence = {
-    "/persist" = {
-    hideMounts = true;
-      directories = [
-        "/var/lib/acme"
-      ];
-    };
+    	sops.secrets = {
+    	  cloudflare_email = {
+    	    sopsFile = ../../../../atreides/secrets.yaml;
+    	  };
+    	  cloudflare_api_key = {
+    	    sopsFile = ../../../../atreides/secrets.yaml;
+    	  };
+    	};
+    	networking.firewall.allowedTCPPorts = [ 80 443 ];
+    
+    	security.acme = {
+    		acceptTerms = true;
+    		defaults = {
+    			email = "alex@mcculley.tech";
+    			dnsProvider = "cloudflare";
+    			credentialFiles = {
+    				CLOUDFLARE_EMAIL_FILE = config.sops.secrets.cloudflare_email.path;
+    				CLOUDFLARE_API_KEY_FILE = config.sops.secrets.cloudflare_api_key.path;
+    			};
+    		};
+    		certs = {
+    			"home.mcculley.tech" = {
+    			  inheritDefaults = true;
+    			  domain = "*.home.mcculley.tech";
+    			  extraDomainNames = ["home.mcculley.tech"];
+    			};
+    		};
+    	};
+    
+    	environment.persistence = {
+        "/persist" = {
+        hideMounts = true;
+          directories = [
+            "/var/lib/acme"
+          ];
+        };
+      };
   };
 }
